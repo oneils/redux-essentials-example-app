6f77572b8f183c1be97ac06115e273d4fe07195c
oneils <lex.nox@gmail.com>
2022-07-03 13:46:32 +0200

    basic redux dataflow

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+import {useDispatch} from "react-redux";
+import {nanoid} from "@reduxjs/toolkit";
+import {postAdded} from "./postsSlice";
+
+export const AddPostForm = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const dispatch = useDispatch()
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onContentChanged = e => setContent(e.target.value)
+
+    const onSavePostClicked = () => {
+        if (title && content) {
+            dispatch(
+                postAdded({
+                  id: nanoid(),
+                  title,
+                  content
+                })
+            )
+
+            setTitle('')
+            setContent('')
+        }
+    }
+    return (
+        <section>
+            <h2>Add a new Post</h2>
+            <form>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    value={title}
+                    name="postTitle"
+                    onChange={onTitleChanged}
+                />
+
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    value={content}
+                    name="postContent"
+                    onChange={onContentChanged}
+                    />
+                <button type="button" onClick={onSavePostClicked}>Save Post</button>
+            </form>
+        </section>
+    )
+}
