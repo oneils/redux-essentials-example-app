e36e1a8d9aed8e16371f94f4618d1104cb0208dc
oneils <lex.nox@gmail.com>
2022-07-04 19:19:03 +0200

    Added reactions buttons

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,13 +2,18 @@ import React from 'react'
 import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
+import {TimeAgo} from "./TimeAgo";
+import {ReactionButtons} from "./ReactionButtons";
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts);
 
-    const renderedPosts = posts.map(post => (
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const renderedPosts = orderedPosts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
+            <ReactionButtons post={post}/>
             <PostAuthor userId={post.user} />
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className="button muted-button">View More</Link>
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
+import {ReactionButtons} from "./ReactionButtons";
 
 export const SinglePostPage = ({match}) => {
     const {postId} = match.params
@@ -18,6 +19,7 @@ export const SinglePostPage = ({match}) => {
         <section>
             <article className="post">
                 <h2>{post.title}</h2>
+                <ReactionButtons post={post}/>
                 <PostAuthor userId={post.user} />
                 <p className="post-content">{post.content}</p>
                 <Link to={`/editPost/${post.id}`} className="button muted-button">Edit Post</Link>
